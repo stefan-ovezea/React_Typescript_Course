46cf4dfb9f77ca5c4f392dc78dd8f78bf58662ea
Stefan Ovezea <stefan.ovezea@iss-shipping.com>
2023-10-20 13:25:58 +0300

    Added namespaces

---
+++ typescript-101/src/07_namespaces.ts
@@ -0,0 +1,12 @@
+console.log('\n=================== NAMESPACES START ===================');
+
+export namespace React {
+  export class Component<Props> {
+    constructor(props: Props) {
+      // React magic
+    }
+  }
+
+}
+
+console.log('==================== NAMESPACES END ====================');
